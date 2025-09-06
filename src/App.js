fb451784d1e01902282f368085ba3915fd07f436
Subhan Osmanov <subhanosmanov625@example.com>
2025-09-06 18:50:19 +0400

    Dəyişiklik haqqında qısa mesaj

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Subhan Osmanov
         </a>
       </header>
     </div>
